1f52aa474305a49e7c9cae3a092d576b9613ec02
Kaare Hoff Skovgaard <kaare@kaareskovgaard.net>
2015-12-28 16:55:22 +0100

    Hack Assert class, to perform various assertions, more to come

---
+++ src/Assert.php
@@ -0,0 +1,20 @@
+<?hh // strict
+
+namespace GraphQL;
+
+abstract final class Assert {
+	public static function isNonNull<T>(?T $val) : T {
+		if (is_null($val)) {
+			throw new AssertException("Value was expected to be non null");
+		}
+		return $val;
+	}
+
+	public static function isInstanceOf<T>(mixed $val,	classname<T> $class) : T {
+		if (!($val instanceof $class)){
+			throw new AssertException("Expected value to be instance of $class");
+		}
+
+		return $val;
+	}
+}
---
+++ src/AssertException.php
@@ -0,0 +1,5 @@
+<?hh // strict
+
+namespace GraphQL;
+
+class AssertException extends \Exception {}
